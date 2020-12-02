c6a0be6f477877fd7b2bb97f150919ad47528ed2
Amandine <amandine.fradet@live.fr>
2020-12-02 18:08:15 +0100

    Update de key pour la methode use()

--- Key.java
+++ Key.java
@@ -9,17 +9,17 @@ public class Key extends Item implements Usable{
 	 * @param name
 	 */ 
 	public Key(String name) {
-		super(name, true);
+            super(name, true);
 	}
         
         @Override
 	public void use() {
-           
+            System.out.println("You're using the " + this.getName() +".");
 	}
 
 	@Override
-	public void print(){
-            System.out.println("This " + this.getName() + "is usable");
+	public void print() {
+            System.out.println("This " + this.getName() + "is usable.");		
 	}
 
 }
