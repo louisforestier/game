35009da7cf1b193ffe2e6ed28124f206db432371
louisf <louis8753@gmail.com>
2020-12-03 18:29:34 +0100

    Update Key.java

--- Key.java
+++ Key.java
@@ -1,39 +1,34 @@
 package game;
 
 /**
- *
  * @author name
  */
-public class Key extends Item implements Usable{
-	/**
-	 * @param name
-	 */ 
+public class Key extends Item implements Usable {
+    /**
+     * @param name
+     */
     public Key(String name) {
         super(name, true);
     }
-    
+
     @Override
-    public void use()
-    {
+    public void use() {
         System.out.println("You can't use this alone.");
     }
-        
+
     @Override
     public void use(Receiver obj) {
-        if(obj instanceof Lockable)
-        {
-               obj.receive(this);
-        }
-        else 
-        {
+        if (obj instanceof Lockable) {
+            obj.receive(this);
+        } else {
             System.out.println("You can't use this with this object.");
-	}
-        
-    }            
+        }
 
-	@Override
-	public void print() {
-            System.out.println("This " + this.getName() + "is usable.");		
-	}
+    }
+
+    @Override
+    public void print() {
+        System.out.println("This " + this.getName() + "is usable.");
+    }
 
 }
