8ed7f802c76c904e250420fb02cb196c21c67bad
louisf <louis8753@gmail.com>
2020-12-07 15:26:40 +0100

    Update GreatSword.java
    correction du nom

--- GreatSword.java
+++ GreatSword.java
@@ -1,15 +1,15 @@
 package game;
 
-public class GreatSword extends Weapon {
+public class Greatsword extends Weapon {
 
 	private final static int DEFAULT_ATTACK_POWER = 12;
 
-	public GreatSword(String name, String description) {
-		super(name, description, GreatSword.DEFAULT_ATTACK_POWER);
+	public Greatsword(String name, String description) {
+		super(name, description, Greatsword.DEFAULT_ATTACK_POWER);
 	}
 
-	public GreatSword(String name, String description, int attackPowerBonus) {
-		super(name, description, GreatSword.DEFAULT_ATTACK_POWER + attackPowerBonus);
+	public Greatsword(String name, String description, int attackPowerBonus) {
+		super(name, description, Greatsword.DEFAULT_ATTACK_POWER + attackPowerBonus);
 	}
 
 }
