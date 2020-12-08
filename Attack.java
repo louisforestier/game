6201e201dbb93031d163f3e9d52c0b8962e649f8
louisf <louis8753@gmail.com>
2020-12-08 23:14:46 +0100

    Update Attack.java
    découpage de launchcommand en sous fonction, c'est tout de suite plus lisible

--- Attack.java
+++ Attack.java
@@ -20,41 +20,49 @@ public class Attack extends Command {
         return (Attackable) hero.getPlace().getInteractions().get(name);
     }
 
+    public void startCombat(NonPlayerCharacter npc){
+        if (!npc.isHostile()){
+            npc.setHostile(true);
+            System.out.println("Hey ! What have I done to deserve this ?!");
+        }
+        Combat c = new Combat(this.hero, this.hero.getPlace().getEnemiesInPlace(),this.scanner);
+        this.hero.setOngoingCombat(c);
+        this.hero.getOngoingCombat().runCombat(this.hero);
+    }
+
+    public void attackInCombat(NonPlayerCharacter npc){
+        this.hero.attack(this.hero.getOngoingCombat().getEnemies().get(npc.getName()));
+    }
+
+    public void attackNonPlayerCharacter(NonPlayerCharacter npc){
+        if (npc.isAlive()) {
+            if (this.hero.getOngoingCombat() == null) {
+                this.startCombat(npc);
+            } else this.attackInCombat(npc);
+        } else {
+            System.out.println("You're attacking a dead body...");
+            System.out.println("God, what is wrong with you ?");
+            System.out.println("Are you trying to get on my nerves ?! Because you're successfull !");
+        }
+    }
+
     @Override
     public void launchCommand(List<String> argument) throws InvalidArgumentNumberException, ClassCastException, NullPointerException{
         if (argument.size() == Attack.NB_ARG){
             if (!(argument.get(0).equals(hero.getName()))) {
                 Attackable a = stringToAttackableInPlace(argument.get(0));
                 if (a instanceof NonPlayerCharacter ){
-                    NonPlayerCharacter npc = (NonPlayerCharacter)a;
-                    if (npc.isAlive()) {
-                        if (this.hero.getOngoingCombat() == null) {
-                            if (!npc.isHostile()){
-                                npc.setHostile(true);
-                                System.out.println("Hey ! What have I done to deserve this ?!");
-                            }
-                            ((NonPlayerCharacter) a).setHostile(true);
-                            Combat c = new Combat(this.hero, this.hero.getPlace().getEnemiesInPlace(),this.scanner);
-                            this.hero.setOngoingCombat(c);
-                            this.hero.getOngoingCombat().runCombat(this.hero);
-
-                        } else this.hero.attack(this.hero.getOngoingCombat().getEnemies().get(argument.get(0)));
-                    } else {
-                        System.out.println("You're attacking a dead body...");
-                        System.out.println("God, what is wrong with you ?");
-                        System.out.println("Are you trying to get on my nerves ?! Because you're successfull !");
-                    }
+                    this.attackNonPlayerCharacter((NonPlayerCharacter) a);
                 } else hero.attack(a);
             } else {
                 System.out.println("You want to hurt yourself ?");
                 System.out.println("I mean, I'm not judging or anything but that's pretty messed up.");
             }
-
         } else throw new InvalidArgumentNumberException();
     }
 
     @Override
     public void help() {
-        System.out.println("attack enemy_name");
+        System.out.println("attack npc_name");
     }
 }
